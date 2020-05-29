3089cbc8a4497ccd8974a9742e51390190ebb00e
LZ <lzhang74@binghamton.edu>
2020-05-28 22:44:14 -0700

    update style

--- js/app.js
+++ js/app.js
@@ -1,26 +1,29 @@
-window.onload = function(){
-    
-
-  var navbar = document.getElementById("navbar__list")
-  var elements = document.getElementsByTagName("section")
-  var smallestNumber = screen.height
-  var smallestId = 'start'
-  
-
-  for (let i=0; i < elements.length; i++) {
-      var list = document.createElement("LI")
-      var li_name = document.createTextNode(elements[i].dataset.nav)
-      let id = elements[i].id
-      list.appendChild(li_name)
-      
-      list.onclick = function(){
-          jump(id);
-      }
-      navbar.appendChild(list)
-      
-  }
-}()
-
 function jump(id){
   document.getElementById(id).scrollIntoView({behavior: "smooth"})
 }
+
+// Tabs setup
+
+
+function openCity(evt, cityName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+
