2ef7cc22d23a136ae66377234001f6a05e2de996
LZ <lzhang74@binghamton.edu>
2020-06-01 23:33:16 -0700

    show pictures in grid form. Currently set to three images to a row.

--- js/app.js
+++ js/app.js
@@ -23,7 +23,7 @@ function openTab(evt, cityName) {
   }
 
   // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(cityName).style.display = "block";
+  document.getElementById(cityName).style.display = "flex";
   evt.currentTarget.className += " active";
 
   
